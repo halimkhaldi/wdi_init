ca7dab05e9fdec5348d8ae77cced73857244ae36
Abdelhalim <golden-boy1994@hotmail.fr>
2019-12-03 21:57:46 -0700

    fixed version issues

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 var shell=require('shelljs');
+shell.config.silent = true; // Don't let echo print to the terminal
 var args=[];
 var types=["String","Number","Boolean","Date"];
 function cap(string) {
@@ -16,20 +17,23 @@ if ( shell.mkdir(project)){
   shell.echo("making basinc structer /views && /models && /public");
   shell.mkdir(`${project}/views`,`${project}/public`,`${project}/public/scripts`,`${project}/public/styles`,`${project}/models`);
   shell.echo("creating a models/index.js")
-shell.exec(` echo "var mongoose = require('mongoose');
+shell.echo(`var mongoose = require('mongoose');
 /*add you connection somewhere here*/
-mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost/${project}', {promiseLibrary: global.Promise});
-" >> ${project}/models/index.js`);
+mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost/${project}', {promiseLibrary: global.Promise},function(error,success){
+  if(error) console.log(error);
+      console.log("connection successful");
+      });
+`).toEnd(`${project}/models/index.js`);
 shell.echo("creating script and style");
 shell.touch(`${project}/public/scripts/app.js`);
 shell.touch(`${project}/public/scripts/app.js`);
 shell.touch(`${project}/public/styles/style.css`);
-shell.echo("server .js");
+shell.echo("server.js");
 shell.touch(`${project}/server.js`);
 shell.touch(`${project}/seed.js`);
 shell.echo("making json package");
 shell.touch(`${project}/package.json`);
-shell.exec(`echo '{
+let obj =shell.echo( `{
   "name": "${project}",
   "version": "1.0.0",
   "description": "",
@@ -45,9 +49,9 @@ shell.exec(`echo '{
     "nodemon": "^1.17.1",
     "body-parser": "^1.18.2"
   }
-}'>> ${project}/package.json`);
+}`).toEnd(`${project}/package.json`);
 shell.echo('basic server.js');
-shell.exec(`echo "var express=require('express');
+shell.echo(`var express=require('express');
 var app=express();
 var db=require('./models/index.js');
 var bodyParser = require('body-parser');
@@ -66,20 +70,20 @@ app.use(function(req, res, next) {
 
 //basic root route
 app.get('/',function(req,res){
-  console.log('do you stuff here')
-  });
+  res.json({"msg":"server is running"})
+});
 
 
 
 app.listen(process.env.PORT || 3000,function(){
   console.log('server running');
-});" >> ${project}/server.js`);
-shell.exec(`echo "var db=require('./models/index.js');" >> ${project}/seed.js`);
-shell.exec(`echo 'web: node server.js' >> ${project}/Procfile`);
+});`).toEnd(`${project}/server.js`);
+shell.echo(`var db=require('./models/index.js');`).toEnd(`${project}/seed.js`);
+shell.echo(`web: node server.js`).toEnd( `${project}/Procfile`);
 shell.exec(`cd ${project}/ && npm install`);
-shell.exec(`echo "node_modules" >> ${project}/.gitignore`);
+shell.echo(`node_modules`).toEnd(`${project}/.gitignore`);
 shell.exec(`cd ${project}/ && git init && git add -A && git commit -m 'initialized project ${project}'`);
-shell.echo(`echo "project created run cd ${project} no need to run npm isntall I already did that for you";`)
+shell.echo(`project created run cd ${project} no need to run npm isntall I already did that for you`)
 
 }else{
   shell.echo("project already exists")
@@ -91,27 +95,28 @@ shell.echo(`echo "project created run cd ${project} no need to run npm isntall I
 	if(args[3] =='model'){
   var i=5;
   var model=cap(args[4]);
+  var lol = shell.echo("woow");
   shell.echo(`creating model ${model}` );
-  shell.exec(`echo "var mongoose = require('mongoose');
+  shell.echo(`var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-${model}Schema = new Schema({" >> models/${model}.js`);
+${model}Schema = new Schema({`).toEnd(`models/${model}.js`);
   for (i;i<args.length-1;i++){
 	var option=args[i].split(':');
 	option[1] = option[1] || 'String';
 	option[1]=  (types.includes(cap(option[1]))) ? cap(option[1]) : 'String';
 
-    shell.exec(`echo "${option[0]}:${option[1]}," >> models/${model}.js`);
+    shell.echo(`${option[0]}:${option[1]},`).toEnd(`models/${model}.js`);
   }
 	var option=args[i].split(':');
 	option[1] = option[1] || 'String';
 	option[1]= (types.includes(cap(option[1]))) ? cap(option[1]) : 'String';
-  shell.exec(`echo "${option[0]}:${option[1]}" >> models/${model}.js`);
-  shell.exec(`echo "});
+  shell.echo(` ${option[0]}:${option[1]}`).toEnd(`models/${model}.js`);
+  shell.echo(`});
 var ${model} = mongoose.model('${model}', ${model}Schema);
-module.exports = ${model};" >> models/${model}.js`);
-shell.exec(`echo "/* adding model ${model} to index.js */
-module.exports.${model} = require('./${model}');">> models/index.js;`);
+module.exports = ${model};`).toEnd(`models/${model}.js`);
+shell.echo(` /* adding model ${model} to index.js */
+module.exports.${model} = require('./${model}');`).toEnd(`models/index.js`);
 
 }else
 console.log("wrong entry you may use -g model or -g controller")
